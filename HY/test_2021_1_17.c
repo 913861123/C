9b8d05d3feadc988d76a4e92f82a396f10e5d954
何翰宇 <913861123@qq.com>
2021-01-19 23:04:01 +0800

    提交文件

---
+++ HY/test_2021_1_17.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main() {
+	char password[16] = { 0 };
+	printf("请输入密码->");
+	scanf("%s", password);//数组本来就是地址，所以不用取址符
+	int ch = 0;//因为getchar可能会返回EOF,EOF=-1所以要用int
+	while (ch = getchar() != '\n') {//清除缓冲区
+	}
+	printf("确认密码Y/N ");
+	ch = getchar();//获取一个字符
+	if (ch == 'Y') {
+		printf("密码输入成功\n密码为：%s", password);
+	}
+	else if (ch == 'N') {
+		printf("密码输入取消\n");
+	}
+	else {
+		printf("输入错误");
+	}
+	return 0;
