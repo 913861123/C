d2f0f86333870719eebb9ff0bfad73d6c6119967
何翰宇 <913861123@qq.com>
2021-01-20 14:51:45 +0800

    二分查找

--- HY/test_2021_1_19.c
+++ HY/test_2021_1_19.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main(){
-	int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+	int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };//二分查找只能用于一组有序数字查找
 	int cd = sizeof(arr) / sizeof(arr[0]);//sizeof计算的是数组的大小，单位是字节。
 	int left = 0;//左下标
 	int right = cd - 1;//右下标
@@ -9,10 +9,10 @@ int main(){
 	scanf("%d",&x);
 	while (left <= right){
 		int mid = left+(right-left)/ 2;//每次二分查找都要从新确定范围，所以要把变量定义到循环里面
-		if (arr[mid] > x){
+		if (arr[mid] > x){//当找的那个数比中间那个数小时
 			right = mid - 1;
 		}
-		else if (arr[mid] < x){
+		else if (arr[mid] < x){//当找那个数比中间那个数大时
 			left = mid + 1;
 		}
 		else {
