b30f5c57cfdf83d67537e2ccd76953036155c7a5
何翰宇 <913861123@qq.com>
2021-01-21 19:56:45 +0800

    函数调用输出100到200之间的素数

---
+++ HY/test_2021_1_21.2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <math.h>
+int shusu(int i){
+	for (int j = 2; j <= sqrt(i); j++){////如果i是素数那么2到根号i中一定有个数字能整除i
+		if (i%j== 0){
+			return 0;
+		}
+	}
+	return 1;
+}
+int main(){
+	int i = 0;
+	for (int i = 100; i <= 200; i++){
+		shusu(i);
+		if (shusu(i) == 1){
+			printf("%d\n", i);
+		}
+	}
+	return 0;
+}
