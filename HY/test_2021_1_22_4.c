9c969a95780555c2528f533d9c8bf989951db06f
何翰宇 <913861123@qq.com>
2021-01-23 13:33:10 +0800

    递归求n的阶层

--- HY/test_2021_1_22_4.c
+++ HY/test_2021_1_22_4.c
@@ -1,17 +1,20 @@
 #include <stdio.h>
 
 int Fac(int n){
-	if (n <= 1)
+	if (n <= 1){
 		return 1;
-	else
-		return n*Fac(n - 1);//n*(n-1)求n的阶层
-	
+	}
+	else {
+		return n*Fac(n - 1);//当n大于1的时候继续递归
+	}
 }
 
 int main(){
-	int x = 0;
-	scanf("%d", &x);
-	int sum=Fac(x);
+	int n = 0;
+	printf("输入一个整数求它的阶层\n");
+	scanf("%d", &n);
+	Fac(n);
+	int sum = Fac(n);
 	printf("%d\n", sum);
 	return 0;
 }
