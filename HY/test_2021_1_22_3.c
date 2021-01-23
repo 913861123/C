49ec870083713037231ce79f095ee9361c7eb4ac
何翰宇 <913861123@qq.com>
2021-01-23 13:36:30 +0800

    递归求字符串长度

--- HY/test_2021_1_22_3.c
+++ HY/test_2021_1_22_3.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int su(char* arr){//数组传过来的是首元素的地址
 	if (*arr != '\0'){//
-		return 1 + su(arr+1);//当arr读取的不是\0时下标加一
+		return 1 + su(arr+1);//当arr读取的不是\0时下标加1
 	}
 	else{
 		return 0;
