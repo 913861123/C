56e233b5400b9307e178f8e50f1467bb4a6e5cc3
何翰宇 <913861123@qq.com>
2021-02-04 22:18:51 +0800

    模拟实现strstr函数

---
+++ HY/test_2021_2_4_4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <assert.h>
+
+char* my_strstr(const char* s1, const char* s2)
+{
+	assert(s1 && s2);
+	const char* sp = s1;
+	
+	while (*sp)//当sp没有指向\0时
+	{
+		const char* p1 = sp;
+		const char* p2 = s2;
+		while ((*p1!=NULL)&&(*p2!=NULL)&&(*p1==*p2))
+		{
+			p1++;
+			p2++;
+		}
+		if (*p2 == '\0')
+		{
+			return (char*)sp;
+		}
+		sp++;
+	}
+	return NULL;
+}
+
+
+int main()
+{
+	char arr1[] = "peaceS";
+	char arr2[] = "ace";
+	printf("%s",my_strstr(arr1, arr2));
+
+	return 0;
+}
