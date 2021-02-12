55af73d0fcfec5bff832a25a2568956f586cc7fc
何翰宇 <913861123@qq.com>
2021-02-12 21:50:26 +0800

    模拟实现memmove函数

--- HY/test_2021_2_11.c
+++ HY/test_2021_2_11.c
@@ -1,32 +1,40 @@
-void * memmove ( void * dst, const void * src, size_t count)
+#include <stdio.h>
+#include <string.h>
+#include <assert.h>
+
+void* my_memmove(void* dest, const void* src, size_t count)
 {
-  void * ret = dst;
-​
-  if (dst <= src || (char *)dst >= ((char *)src + count)) {
-    /*
-    * Non-Overlapping Buffers
-    * copy from lower addresses to higher addresses
-    */
-    while (count--) {
-        *(char *)dst = *(char *)src;
-        dst = (char *)dst + 1;
-        src = (char *)src + 1;
-   }
- }
-  else {
-    /*
-    * Overlapping Buffers
-    * copy from higher addresses to lower addresses
-    */
-    dst = (char *)dst + count - 1;
-    src = (char *)src + count - 1;
-​
-    while (count--) {
-        *(char *)dst = *(char *)src;
-        dst = (char *)dst - 1;
-        src = (char *)src - 1;
-   }
- }
-​
-  return(ret);
+	assert(dest && src);
+
+	if (dest < src)
+	{//从前向后拷贝
+		//以src的起始地址为分界线，如果dest的起始地址小于src起始的地址
+		while (count--)
+		{
+			*(char*)dest = *(char*)src;
+			++(char*)dest;
+			++(char*)src;
+		}
+	}
+	else//如果dest的起始地址等于或大于src的地址
+	{//从后向前拷贝
+		
+		while (count--)
+		{
+			*((char*)dest+count) = *((char*)src+count);
+		}
+	}
+}
+
+int main()
+{
+	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
+	my_memmove(arr+1,arr,16 );
+	
+	int i = 0;
+	for (i = 0; i < sizeof(arr) / sizeof(arr[0]); i++)
+	{
+		printf("%d ", arr[i]);
+	}
+	return 0;
 }
