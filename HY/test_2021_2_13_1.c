61acf06ac76ad9ec6e2aa480526f2be9afd64172
何翰宇 <913861123@qq.com>
2021-02-13 20:54:13 +0800

    模拟实现memmove函数

---
+++ HY/test_2021_2_13_1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+void* my_memcpy(void* dest, const void* src, size_t count)
+{
+	while (count)
+	{
+		*((char*)dest) = *((char*)src);
+		++(char*)dest;
+		++(char*)src;
+		--count;
+	}
+}
+
+int main()
+{
+	char arr[100] = "hello ";
+	char arr2[] = "word !";
+	my_memcpy(arr+6, arr2, 6);
+	int i = 0;
+	for (i = 0; i < sizeof(arr) / sizeof(arr[0]); i++)
+	{
+		printf("%c", arr[i]);
+	}
+	return 0;
+}
