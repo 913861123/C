62856a5b6091c4e687da2ba8494de80ef238e784
何翰宇 <913861123@qq.com>
2021-02-19 23:49:32 +0800

    求数组里的最大数

--- HY/test_2021_1_18.c
+++ HY/test_2021_1_18.c
@@ -2,9 +2,11 @@
 int main(){
 	int arr[10] = { 0 };
 	int max = 0;//定义一个max变量，用来和数组里每一个数比大小 
-	while (scanf("%d",&arr) != EOF){
+	while (scanf("%d",&arr) != EOF)
+	{
 		for (int i = 0; i<sizeof(arr) / sizeof(arr[0]); i++){
-			if (max<arr[i]){//每当max小于数组里某一个数时就把这个数赋值给max 
+			if (max<arr[i])//每当max小于数组里某一个数时就把这个数赋值给max
+			{ 
 				max = arr[i];
 			}
 		}
