f7e20463d565be1ce28d408244893698796b924f
何翰宇 <913861123@qq.com>
2021-02-22 20:17:59 +0800

    非递归求第n个斐波那契额数

---
+++ 算法题/test_2021_2_22_1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int Fibonacci(int n)
+{
+	int a = 1;
+	int b = 1;
+	int c = 1;
+	
+	while (n > 2)
+	{
+		c = a + b;
+		a = b;
+		b = c;
+		--n;
+	}
+	return c;
+}
+
+int main()
+{
+	int n = 0;
+	printf("请输入要求的第n个斐波那契数\n");
+	scanf("%d", &n);
+	printf("第%d个斐波那契数为%d",n,Fibonacci(n));
+	return 0;
+}
