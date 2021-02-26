d9159609af8376e42e20b8c00942f364e2c970a3
何翰宇 <913861123@qq.com>
2021-02-26 22:16:41 +0800

    求数组里个数超过数组长度一半的数字

--- 算法题/test_2021_2_25_1.c
+++ 算法题/test_2021_2_25_1.c
@@ -1,37 +1,81 @@
 #include <stdio.h>
+#include <assert.h>
 
-int half(int* arr, int sz)
+int half(int* arr, int len)//只能用于数组里有个数超过数组长度一半的数组
 {
+	assert(arr != NULL && len > 0);
 	int conte = 0;
+	int tmp = 0;
+	int i = 0;
+
+	for (i = 0; i < len; i++)
+	{
+
+		if (0 == conte)//当为0时更新数字
+		{
+			tmp = arr[i];
+			conte = 1;
+		}
+		else if (tmp == arr[i])//相等加一
+		{
+			conte++;
+		}
+		else//不相等减一
+		{
+			conte--;
+		}
+	}
+
+	return tmp;
+}
+
+int* halfs(int* arr, int len)//只有当数组里有超过长度一半的数才会返回数字
+{
+	assert(arr != NULL && len > 0);
 	int i = 0;
 	int j = 0;
-	for (i = 0; i < sz/2; i++)
+	for (i = 0; i < len / 2; i++)
 	{
-		conte = 1;
-		for (j = i+1; j <sz-i; j++)
+		int conte = 1;//计算数字出现的个数
+		int tmp = arr[i];
+		for (j = i + 1; j <= len; j++)
 		{
-			if (arr[i] == arr[j])
+
+			if (tmp == arr[j])
 			{
 				conte++;
 			}
-			
+			if (tmp > len / 2)//当数字个数超过数字的一时直接返回那个数字的地址
+			{
+				return &tmp;
+			}
 		}
-		if (conte > sz / 2)
-		{
-			return arr[i];
-		}
-		
 	}
-		return 0;
+
+	return NULL;
 }
 
+void test1()
+{
+	int arr[] = { 1,2,5,3,4,5,5,5,5 };
+	int* ret = half(arr, sizeof(arr) / sizeof(arr[0]));
+	if (ret != NULL)
+	{
+		printf("%d\n", (int)ret);
+	}
+}
+
+void test2()
+{
+	int arr[] = {3,2,1,3,3,2,2,3,3,1};
+	int ret = half(arr, sizeof(arr) / sizeof(arr[0]));
+	printf("%d\n", ret);
+
+}
 int main()
 {
-	int arr[] = { 1,2,5,3,4,5,5,5,5};
-	int ret = half(arr, sizeof(arr) / sizeof(arr[0]));
-	if (ret != 0)
-	{
-		printf("%d", ret);
-	}
+	test1();
+	test2();
+
 	return 0;
 }
