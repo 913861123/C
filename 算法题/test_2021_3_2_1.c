1c9d116f4e87ee50d50eb7008bb95c457e90f794
何翰宇 <913861123@qq.com>
2021-03-04 20:34:10 +0800

    找出字符串中第一个只出现一次的字符

--- 算法题/test_2021_3_2_1.c
+++ 算法题/test_2021_3_2_1.c
@@ -4,38 +4,39 @@
 char one_char(const char* ch, int leng)
 {
 	assert(ch != NULL && leng > 0);
-	if (1 ==  leng)
-	{
-		return ch[0];
-	}
+
 	int i = 0;
 	int j = 0;
-	int cont = 0;
-	for (i = 0; i < leng-1; i++)
+	int conte = 0;
+
+	for (i = 0; i < leng - 1; i++)
 	{
-		cont = 0;
-		for (j = i + 1; j < leng - i-1; j++)
+		conte = 0;
+		for (j =0; j < leng - 1; j++)
 		{
-			if (ch[i] == ch[j])
+			if (i != j)
 			{
-				cont++;
-				
+				if (ch[i] == ch[j])//当有相同的字符时直接跳出循环进行下一轮判断
+				{
+					++conte;
+					break;
+				}
 			}
+			
 		}
-		if (0==cont)
+		if (0 == conte)
 		{
 			return ch[i];
 		}
-
 	}
+
 }
 
 int main()
 {
-	char ch[] = "abaccdeff";
+	char ch[] = "babaccdeff";
 	char ret = one_char(ch, sizeof(ch) / sizeof(ch[0]));
 
 	printf("%c", ret);
-	
 	return 0;
 }
