be0ced2ef4040fb091e6330b123d1388aec566a9
何翰宇 <913861123@qq.com>
2021-03-07 22:16:31 +0800

    逆序字符串和字符串里的每一个单词

--- 算法题/test_2021_3_6_1.c
+++ 算法题/test_2021_3_6_1.c
@@ -2,35 +2,49 @@
 #include <string.h>
 #include <assert.h>
 
-void reversal(char* left,char* right)
+void reversal(char* left, char* right)
 {
 	assert(left && right);
 	while (left < right)
 	{
-		
 		char tmp = *left;
 		*left = *right;
 		*right = tmp;
-		left++;
-		right--;
+		++left;
+		--right;
 	}
 }
+
 int main()
 {
-	char ch[] = "hello bei jin.";
-
-	reversal(ch, ch+strlen(ch)-1);
+	char ch[] = "hello bei jing.";
+	int len = strlen(ch);
+	reversal(ch,ch+len-1);//逆序字符串
 
 	char* s1 = ch;
 	char* s2 = ch;
-	printf("%s", ch);
-
-	while (*s2 != ' ')
+	
+	while (*s1)
 	{
-		++s2;
+		s2 = s1;
+		while (*s2 != ' '&& *s2 != '\0')//当s2没指向空格时
+		{
+			++s2;
+		}
+		reversal(s1, s2 - 1);//逆序每一个单词
+		if (*s2 == ' ')
+		{
+			s1 = s2 + 1;
+		}
+		else//等于、0时
+		{
+			s1 = s2;
+		}
+
 	}
-	reversal(s1, s2);
-	s2 = s2 + 1;
+	
+	printf("%s\n", ch);
 
 	return 0;
+
 }
