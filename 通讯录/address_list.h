8c34d63c228095aaa1ebe6c2098ec4c10cb63379
何翰宇 <913861123@qq.com>
2021-03-10 23:37:27 +0800

    通讯录

---
+++ 通讯录/address_list.h
@@ -0,0 +1,44 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+
+#define MAX 100
+#define NAME_MAX 10
+#define SEX_MAX 4
+#define PHONE_MAX 12
+#define ADDRESS_MAX 30
+
+//存放联系人信息的结构体
+struct information
+{
+	char name[NAME_MAX];
+	char sex[SEX_MAX];
+	int age;
+	char phone[PHONE_MAX];
+	char address[ADDRESS_MAX];
+};
+
+//存放联系人的结构体
+struct contact
+{
+	struct information data[MAX];//存放100个联系人
+	int people;//统计人的个数
+};
+
+//初始化通讯录
+void Initialize(struct contact* pc);
+
+//添加联系人
+void AddContact(struct contact* pc);
+
+//打印联系人
+void ShowContact(const struct contact* pc);
+
+//删除联系人
+void DeleteContact(struct contact* pc);
+
+//修改联系人
+void AmendContact(struct contact* pc);
+
+//查找联系人
+void FindContact(const struct contact* pc);
---
+++ 通讯录/test.c
@@ -0,0 +1,71 @@
+#include "address_list.h"
+
+void menu()
+{
+	printf("************************************\n");
+	printf("***** 1.新建联系人 2.打印联系人 ****\n");
+	printf("***** 3.删除联系人 4.修改联系人 ****\n");
+	printf("***** 5.查找联系人 6.排序联系人 ****\n");
+	printf("********** 0.退出通讯录 ************\n");
+}
+
+enum Menu
+{
+	EXIT,
+	ADD,
+	SHOW,
+	DELETE,
+	AMEND,
+	FIND,
+	RANGK
+};
+
+void test()
+{
+	int input = 0;
+	struct contact con;//创建了一个通讯录
+
+	Initialize(&con);
+	do
+	{
+		menu();
+		printf("请输入:>");
+		scanf("%d", &input);
+
+		switch (input)
+		{
+		case ADD:
+			AddContact(&con);
+			break;
+		case SHOW:
+			ShowContact(&con);
+			break;
+		case DELETE:
+			DeleteContact(&con);
+			break;
+		case AMEND:
+			AmendContact(&con);
+			break;
+		case FIND:
+			FindContact(&con);
+			break;
+		case RANGK:
+			break;
+		case EXIT:
+			printf("成功退出通讯录\n");
+			break;
+		default:
+			printf("选择错误，请从新选择\n");
+			break;
+		}
+	} while (input);
+
+}
+
+
+int main()
+{
+	test();
+
+	return 0;
+}
